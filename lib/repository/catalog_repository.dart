11e5f71d37f131ceeab47c47dba2c0486a5a8a93
msAlexG <msgrishkov@gmail.com>
2024-01-15 20:44:21 +0300

    test commmit2

--- lib/repository/catalog_repository.dart
+++ lib/repository/catalog_repository.dart
@@ -1,1 +1,32 @@
+import 'package:sushi_project/models/catalog_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
+class CatalogRepository {
+//CatalogModel catalog =CatalogModel(category: []);
+
+  Future<CatalogModel> GetCatalog() async {
+    var client = http.Client();
+
+    try {
+      var response = await client.get(
+        Uri.parse('https://kr-builder.ru/sushi_test/new.json'),
+      );
+      final decoded = jsonDecode(utf8.decode(response.bodyBytes));
+
+      final List<dynamic> result = decoded['category'];
+
+      final List<Category> categorys = result
+          .map((rawCategorys) => Category.fromJson(rawCategorys))
+          .toList();
+
+      final CatalogModel catalog = CatalogModel(category: categorys);
+
+      //final category = decoded['category'];
+
+      return catalog;
+    } catch (e) {
+      throw Exception("Failed to load category");
+    }
+  }
+}
